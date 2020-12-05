91bb378cc60adcdb0cdf2bc5d673baeb96ce6057
Candra <candrapwr@gmail.com>
2020-12-05 13:30:58 +0700

    Update

--- resources/views/admin/lot/edit.blade.php
+++ resources/views/admin/lot/edit.blade.php
@@ -24,15 +24,22 @@
 		</div>
 	</div>
 	<div class="form-group row">
+		<label class="col-sm-3 control-label text-right"></label>
+		<div class="col-sm-4">
+			<div id="map" style="border:1px solid #0b0;height:450px;width:780px;"></div>
+				<input id="pac-input" class="form-control" style="margin-top:12px;width:60%;" type="text" placeholder="Search Place" size="10">
+			</div>
+	</div>	
+	<div class="form-group row">
 		<label class="col-sm-3 control-label text-right">Lat.</label>
 		<div class="col-sm-4">
-			<input type="text" name="lat" class="form-control" value="<?php echo $modelData->lat ?>" required>
+			<input type="text" name="lat" id="lat" class="form-control" value="<?php echo $modelData->lat ?>" required>
 		</div>
 	</div>	
 	<div class="form-group row">
 		<label class="col-sm-3 control-label text-right">Lng.</label>
 		<div class="col-sm-4">
-			<input type="text" name="lng" class="form-control" value="<?php echo $modelData->lng ?>" required>
+			<input type="text" name="lng" id="lng" class="form-control" value="<?php echo $modelData->lng ?>" required>
 		</div>
 	</div>					
 	<div class="form-group row">
@@ -73,3 +80,153 @@
         <div class="clearfix"></div>
     </div>
 </form>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjbMgc2ARiMViRzHeI6W9pFzLfY8HVdT0&libraries=places&callback=initAutocomplete&hl=id" async defer></script>
+<script>
+	var marker;
+	var map;
+	var myLatLng;
+	var mapDiv;
+	var myPos;
+	var latval;
+	var lngval;
+
+	function initAutocomplete()
+	{
+		var geocoder 	= new google.maps.Geocoder;
+		var infowindow 	= new google.maps.InfoWindow;
+		var zoomset		= 13;
+		
+		// Stayle map
+		var styledMapType = new google.maps.StyledMapType(
+		[
+		{
+			"featureType": "poi.business",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		}
+		],
+		{name: 'Styled Map'});
+		
+		mapDiv = document.getElementById('map');
+		latval = document.getElementById('lat').value;
+		lngval = document.getElementById('lng').value;
+		
+		if (latval == "" && lngval == "") {
+				myLatLng    = {lat: -7.79898677945525, lng: 110.39026800776367};
+				myPos       = null;
+			zoomset		= 13;
+		} else {
+				myLatLng    = {lat: Number(latval), lng: Number(lngval)};
+				myPos       = {lat: Number(latval), lng: Number(lngval)};
+			zoomset		= 17;
+		};
+
+		map = new google.maps.Map(mapDiv,
+		{
+				center: myLatLng,
+				zoom: zoomset,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+		});
+		
+		map.mapTypes.set('styled_map', styledMapType);
+		map.setMapTypeId('styled_map');	
+		
+		var input = document.getElementById('pac-input');
+
+		var searchBox = new google.maps.places.SearchBox(input);
+		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+		input.style.display = 'block';
+
+		//event if place changed
+		searchBox.addListener('places_changed', function()
+		{
+			var places = searchBox.getPlaces();
+
+			if (places.length == 0) {
+				return;
+			}
+
+				marker.setMap(null);
+				marker.setPosition(null);
+
+				// For each place, get the icon, name and location.
+				var bounds = new google.maps.LatLngBounds();
+			places.forEach(function(place)
+			{
+					marker.setMap(map);
+					marker.setTitle(place.name);
+
+					if (place.geometry.viewport) {
+					// Only geocodes have viewport.
+					bounds.union(place.geometry.viewport);
+					} else {
+					bounds.extend(place.geometry.location);
+					}
+				});
+
+				map.fitBounds(bounds);
+				//map.setZoom();
+		});
+
+		// default marker
+		marker = new google.maps.Marker({
+				position	: myPos,
+				map			: map,
+				draggable	: true,
+				animation	: google.maps.Animation.DROP,
+				title		: 'Set Position'
+		});
+
+		// add map even listener click
+		google.maps.event.addListener(map, 'click', function(event) {
+				setMarker(event.latLng);
+				var latitude    = event.latLng.lat();
+				var longitude   = event.latLng.lng();
+				setInput(latitude, longitude);
+				
+				geocodeLatLng(geocoder, map, infowindow, latitude, longitude, "#lokasi");
+		});
+
+		// add map even listener dragend marker
+		google.maps.event.addListener(marker, 'dragend', function(event) {
+				setMarker(event.latLng);
+				var latitude    = event.latLng.lat();
+				var longitude   = event.latLng.lng();
+				setInput(latitude, longitude);
+
+				geocodeLatLng(geocoder, map, infowindow, latitude, longitude, "#lokasi");
+		});
+	}
+
+	function setMarker(location) {
+		marker.setPosition(location);
+	}
+
+	function setInput(lat, lng) {
+		var inlat 	= document.getElementById('lat');
+		var inlng 	= document.getElementById('lng');
+		
+		inlat.value = lat;
+		inlng.value = lng;
+	}
+
+	function geocodeLatLng(geocoder, map, infowindow, lat, lng, alamat) {
+		var latlng = {lat: parseFloat(lat), lng: parseFloat(lng)};
+		geocoder.geocode({'location': latlng}, function(results, status) {
+			if (status === 'OK') {
+					if (results[0]) {
+						$(alamat).val(results[0].formatted_address);
+					} else {
+						window.alert('No results found');
+					}
+			} else {
+					window.alert('Geocoder failed due to: ' + status);
+			}
+		});
+	}
+	
+</script>
